58c8f2c1d9db5971948bc364c6bff24b3b956133
xuanronaldo <xuanronaldo@qq.com>
2025-02-01 13:06:32 +0800

    release: v1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="positionbt",
-    version="v1.0.3",
+    version="v1.1.0",
     author="Xuan Ronaldo",
     author_email="xuanronaldo@outlook.com",
     description="PositionBT is a simple, fast, and customizable backtesting framework "
